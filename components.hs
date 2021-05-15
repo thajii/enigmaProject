69606a6129361f30cc2fe4258df2e6932501a05a
Lennart Schrader <lennart.schrader@gmx.de>
2021-05-15 14:38:00 +0200

    implemented Umkehrwalze, umkehr methode, zurueckschluesselung, verschluesselung method

--- components.hs
+++ components.hs
@@ -3,12 +3,13 @@
 alphabet :: [Char]
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
---
+-- Data & Types
 data Walze = Walze {rand_alphabet:: String, klar_alphabet :: String, umspringbuchstabe ::Char} -- umspringbuchstabe ggf als Übergasbeparameter
     deriving(Show)--testen ob die Walze saich drehen lässt mit mittels show/ terminalausgabe
 
 type Walzenkombi = (Walze, Walze, Walze)--hier eine Kombination für das Drehen aller Walzen
 
+type Umkehrwalze = [(Char, Char)]
 
 -- Walzen
 walze1 :: Walze
@@ -32,7 +33,6 @@ dreheWalze (Walze rand_alphabet klar_alphabet umspringbuchstabe) = Walze rand_al
         where rand_alphabet_neu = tail rand_alphabet ++ [head rand_alphabet]
               --klar_alphabet_neu = tail klar_alphabet ++ [head klar_alphabet]-- beide Alphabete müssen verschoben werden: bspw. W1 A=E -> B=K
 
-
 checkObDrehen :: Walze -> Bool--sicher?
 checkObDrehen walze = umspringbuchstabe walze == head (rand_alphabet walze)
 
@@ -50,13 +50,62 @@ sucheInWalze [] _ = '!' -- für Fehler
 sucheInWalze tupelliste c | snd (head tupelliste) == c = fst (head tupelliste)
                           | otherwise = sucheInWalze (tail tupelliste) c
 
-uebersetzten :: Char -> Walze -> Char
-uebersetzten c walze =  sucheInWalze (zip random klar) c-- walze rand_alphabet und klar_alphabet zippen 
+uebersetzen :: Char -> Walze -> Char
+uebersetzen c walze =  sucheInWalze (zip random klar) c-- walze rand_alphabet und klar_alphabet zippen 
                 where random = rand_alphabet walze      -- dann in Hilfsfunktion nach der Stelle suchen, wo Eingabe = KlarBuchstabe
                       klar = klar_alphabet walze        -- dann im durch das Tupel das "zufällige" Gegenstück zurrückgeben.
 
-uebersetztenMitKombi :: Char -> Walzenkombi -> Char
-uebersetztenMitKombi c (w1, w2, w3) = uebersetzten (uebersetzten (uebersetzten c w1) w2) w3-- Buchstaben einmal duch alle Walzen durch übersetzten
+uebersetzenMitKombi :: Char -> Walzenkombi -> Char
+uebersetzenMitKombi c (w1, w2, w3) = uebersetzen (uebersetzen (uebersetzen c w1) w2) w3-- Buchstaben einmal duch alle Walzen durch übersetzten
+
+
+zurueckuebersetzen :: Char -> Walze -> Char
+zurueckuebersetzen c walze = sucheInWalze (zip klar random) c -- genauso wie uebersetzen 
+                        where random = rand_alphabet walze    -- gedacht für den Aufruf nach Verwendung der Umkerwalze
+                              klar = klar_alphabet walze
+
+zurueckuebersetzenMitKombi :: Char -> Walzenkombi -> Char
+zurueckuebersetzenMitKombi c (w1, w2, w3) = zurueckuebersetzen (zurueckuebersetzen (zurueckuebersetzen c w1) w2) w3-- Buchstaben einmal duch alle Walzen durch übersetzten
+
+
+-- Umkehrwalzen
+
+umkehrwalze1 :: Umkehrwalze
+umkehrwalze1 =   zip  "EJMZALYXVBWFCRQUONTSPIKHGD" alphabet--eddmann1
+
+umkehrwalze2 :: Umkehrwalze
+umkehrwalze2 =   zip  "IMETCGFRAYSQBZXWLHKDVUPOJN" alphabet--
+
+-- Umkehrwalzenfunktion
+umkehren :: Char -> Umkehrwalze -> Char 
+umkehren c umkehrwalze | snd(head umkehrwalze) == c = fst (head umkehrwalze)
+                       | otherwise = umkehren c (tail umkehrwalze) 
+
+
+verschluessle :: Char -> Walzenkombi -> Umkehrwalze -> Char
+verschluessle c (w1, w2, w3) u =  zurueckuebersetzenMitKombi (umkehren (uebersetzenMitKombi c (w1, w2, w3)) u) (w1, w2, w3)
+--wo wird gedreht?-> danach
+
+verschluessleString :: String -> Walzenkombi -> Umkehrwalze -> String 
+verschluessleString [] (w1, w2, w3) u = []
+verschluessleString (x:xs) (w1, w2, w3) u = (verschluessle x (w1, w2, w3) u) : (verschluessleString xs (dreheAlleWalzen(w1, w2, w3)) u)
+
+--Für Testzwecke
+main :: IO()
+main = do
+   print (verschluessleString "aaa" (walze1, walze2, walze3) umkehrwalze1)
+
+
+
+
+
+
+
+
+
+
+
+
 
 -- TO-DO Lennart: 
 -- Alles sollte wie gewünscht funktionieren. 
@@ -64,16 +113,17 @@ uebersetztenMitKombi c (w1, w2, w3) = uebersetzten (uebersetzten (uebersetzten c
 -- Nachlesen wie der Reflector funktioniert.
 -- "Spiegelsimulation/funktion" implementieren
 -- Danach kann mit dem Zusammensetzen der Verschlüsselung begonnen werden.
+--"hallo"
+--"xallo"
+--drehen
+--"xullo"
+--drehen
+--"xuplo"
+--drehen
+--"xupzo"
+--drehen
+--"xupzy"
 
 
--- Umkehrwalze
 
-main :: IO()
-main = do
-   print(walze1)
-   -- print("--------------")
-   -- print (dreheWalze (walze1))
-   --print(dreheAlleWalzen(walze1, walze2, walze3))
- 
 
-    --print(preEncodeString[('A','E'),('B','Z')])
